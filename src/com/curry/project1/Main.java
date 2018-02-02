55eaa9dd533415dd1c7c54c16fed7ad7fce40b78
DrahkayCaveDev <33554693+DrahkayCaveDev@users.noreply.github.com>
2018-02-02 17:13:35 -0500

    Update Main.java

--- src/com/curry/project1/Main.java
+++ src/com/curry/project1/Main.java
@@ -9,92 +9,40 @@ import java.util.InputMismatchException;
 public class Main {
     public static void main(String[] args) {
         int choice = -1;
-        Scanner input = new Scanner(System.in);
         List taskList = new ArrayList();
-        String user;
-        int userInt;
 
-        while(choice != 0){
+
+        while (choice != 0) {
             System.out.printf("%n1: Add a Task %n2: Remove a task %n3: Update a task %n4: List all Tasks %n0: Exit");
             try {
                 Scanner scanner = new Scanner(System.in);
                 System.out.printf("%n%nEnter a number: ");
                 choice = scanner.nextInt();
 
-            }catch(InputMismatchException e){
+            } catch (InputMismatchException e) {
+                choice = -100;
             }
 
-            if (choice > 1 && taskList.size() == 0){
+            if (choice > 1 && taskList.size() == 0) {
                 choice = -2;
             }
 
-            switch (choice){
+            switch (choice) {
                 case 1:
-                    System.out.printf("%nWrite a task to add in: ");
-                    user = input.nextLine();
-                    taskList.add(user);
+                    Add(taskList);
                     break;
 
                 case 2:
-                    for (int i = 0; i < taskList.size(); i++) {
-                        System.out.println(i+1 + ": " + taskList.get(i));
-                    }
-
-                    try {
-                        Scanner inputInt = new Scanner(System.in);
-                        System.out.printf("%nChoose a task to remove by typing a number (type zero to not remove anything): ");
-                        userInt = inputInt.nextInt() - 1;
-                    }catch(InputMismatchException e){
-                        userInt = -1;
-                    }
-                    if (userInt <= -1){
-                        break;
-                    }
-                    else if (taskList.size() < userInt + 1) {
-                        System.out.printf("%nHow about actually type in a number given to you.");
-                        break;
-                    }
-                    else{
-                        taskList.remove(taskList.get(userInt));
-                        break;
-                    }
-
+                    Remove(taskList);
+                    break;
 
                 case 3:
-                    for (int i = 0; i < taskList.size(); i++) {
-                        System.out.println(i+1 + ": " + taskList.get(i));
-                    }
-                    try {
-                        Scanner inputInt = new Scanner(System.in);
-                        System.out.printf("%nChoose a task to update by typing a number (type zero to not update anything): ");
-                        userInt = inputInt.nextInt() - 1;
-                    }catch(InputMismatchException e){
-                        userInt = -1;
-                    }
-                    if (userInt <= -1){
-                        break;
-                    }
-                    else if (taskList.size() < userInt + 1) {
-                        System.out.printf("%nHow about actually type in a number given to you.");
-                        break;
-                    }
-                    else{
-                        System.out.printf("%nType in a new task.");
-                        user = input.nextLine();
-                        taskList.remove(userInt);
-                        if (userInt <= 0){
-                            taskList.add(userInt, user);
-                            break;
-                        }
-                        else{
-                            taskList.add(user);
-                            break;
-                        }
-                    }
+                    Update(taskList);
+                    break;
 
                 case 4:
                     for (int i = 0; i < taskList.size(); i++) {
-                        System.out.println(i+1 + ": " + taskList.get(i));
+                        System.out.println(i + 1 + ": " + taskList.get(i));
                     }
                     System.out.printf("%nPress enter to continue ");
                     try {
@@ -114,8 +62,74 @@ public class Main {
             }
 
 
+        }
+    }
 
+    public static List Add(List taskList) {
+        String user;
+        Scanner input = new Scanner(System.in);
+        System.out.printf("%nWrite a task to add in: ");
+        user = input.nextLine();
+        taskList.add(user);
+
+        return taskList;
+    }
+
+    public static List Remove(List taskList) {
+        int userInt;
+        Scanner input = new Scanner(System.in);
+        for (int i = 0; i < taskList.size(); i++) {
+            System.out.println(i + 1 + ": " + taskList.get(i));
+        }
+
+        try {
+            Scanner inputInt = new Scanner(System.in);
+            System.out.printf("%nChoose a task to remove by typing a number (type zero to not remove anything): ");
+            userInt = inputInt.nextInt() - 1;
+        } catch (InputMismatchException e) {
+            userInt = -1;
+        }
+        if (taskList.size() < userInt + 1) {
+            System.out.printf("%nHow about actually type in a number given to you.");
+
+        } else {
+            taskList.remove(taskList.get(userInt));
 
         }
+        return taskList;
+    }
+
+    public static List Update(List taskList) {
+        Scanner input = new Scanner(System.in);
+        String user;
+        int userInt;
+        for (int i = 0; i < taskList.size(); i++) {
+            System.out.println(i + 1 + ": " + taskList.get(i));
+        }
+        try {
+            Scanner inputInt = new Scanner(System.in);
+            System.out.printf("%nChoose a task to update by typing a number (type zero to not update anything): ");
+            userInt = inputInt.nextInt() - 1;
+        } catch (InputMismatchException e) {
+            userInt = -1;
+        }
+        if (userInt <= -1) {
+
+        } else if (taskList.size() < userInt + 1) {
+            System.out.printf("%nHow about actually type in a number given to you.");
+
+        } else {
+            System.out.printf("%nType in a new task.");
+            user = input.nextLine();
+            taskList.remove(userInt);
+            if (userInt >= 0) {
+                taskList.add(userInt, user);
+
+            } else {
+                taskList.add(user);
+
+            }
+        }
+        return taskList;
     }
 }
